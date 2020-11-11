69396bb2620469f2ff2159c47165b271a97ca614
Marcos Costa <50223741+marcoscosta90@users.noreply.github.com>
2020-11-11 17:32:57 -0300

    CustomVideoPlayer

---
+++ CustomVideoPlayer/script.js
@@ -0,0 +1,25 @@
+//pegar os nossos elementos
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+   const method = video.paused ? 'play' : 'pause';
+    video[method]();
+
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
